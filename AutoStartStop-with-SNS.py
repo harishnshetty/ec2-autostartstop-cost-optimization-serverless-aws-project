7c0cccd30306fc74e39de9ebe8a68fc873e07dc2
harishnshetty <harishn662@gmail.com>
2026-01-03 13:13:14 +0530

    sns and readme fixed

--- AutoStartStop-with-SNS.py
+++ AutoStartStop-with-SNS.py
@@ -1,61 +1,56 @@
-
 import boto3
+import json
 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
+    sns = boto3.client('sns')
     
-    SNS_TOPIC_ARN = 'arn:aws:sns:ap-south-1:970378220457:stale-ebs' 
-    NOTIFY_ONLY = False
+    SNS_TOPIC_ARN = 'arn:aws:sns:ap-south-1:970378220457:alb-sns-demo'
     
     tag_key = 'AutoStartStop'
     tag_value = 'True'
 
-    # Retrieve all EC2 instances with the specified tag
+    # Get EC2 instances with tag AutoStartStop=True
     instances = ec2.describe_instances(
         Filters=[{'Name': f'tag:{tag_key}', 'Values': [tag_value]}]
     )
-    
+
     instance_ids = []
     for reservation in instances['Reservations']:
         for instance in reservation['Instances']:
-            instance_id = instance['InstanceId']
-            instance_ids.append(instance_id)  # Fixed variable name
-            
+            instance_ids.append(instance['InstanceId'])
+
     if not instance_ids:
-        print("No instances found with the specified tag AutoStartStop=True.")
-        return {
-            'statusCode': 200,
-            'body': 'No instances found with the specified tag.'
-        }
-    
-    # Get action from event, default to 'stop'
+        message = "No EC2 instances found with AutoStartStop=True"
+        print(message)
+        return {"statusCode": 200, "body": message}
+
     action = event.get('action', 'stop').lower()
-    
+
     if action == 'start':
-        response = ec2.start_instances(InstanceIds=instance_ids)
-        print(f'Started instances: {instance_ids}')
-        
-        if NOTIFY_ONLY:
-            return {
-                'statusCode': 200,
-                'body': f'Successfully started instances: {instance_ids}'
-            }
-        
-        
+        ec2.start_instances(InstanceIds=instance_ids)
+        subject = "EC2 Auto-Start Notification"
+        message = f"Started EC2 instances:\n{instance_ids}"
+
     elif action == 'stop':
-        response = ec2.stop_instances(InstanceIds=instance_ids)
-        print(f'Stopped instances: {instance_ids}')
-        
-        if NOTIFY_ONLY:
-            return {
-                'statusCode': 200,
-                'body': f'Successfully stopped instances: {instance_ids}'
-            }
-        
-        
+        ec2.stop_instances(InstanceIds=instance_ids)
+        subject = "EC2 Auto-Stop Notification"
+        message = f"Stopped EC2 instances:\n{instance_ids}"
+
     else:
-        print("Invalid action specified. Use 'start' or 'stop'.")
         return {
-            'statusCode': 400,
-            'body': "Invalid action specified. Use 'start' or 'stop'."
+            "statusCode": 400,
+            "body": "Invalid action. Use 'start' or 'stop'."
         }
+
+    # 🔔 Send SNS Notification
+    sns.publish(
+        TopicArn=SNS_TOPIC_ARN,
+        Subject=subject,
+        Message=message
+    )
+
+    return {
+        "statusCode": 200,
+        "body": message
+    }
--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# autostartstop-lambda-project
+# Automated AWS EC2 Cost Optimization Framework with EventBridge Scheduler, Lambda, SNS, and IAM
+
+
+
+## For more projects, check out  
+[https://harishnshetty.github.io/projects.html](https://harishnshetty.github.io/projects.html)
+
+[![Video Tutorial](https://github.com/harishnshetty/image-data-project/blob/ccd5b46f956ad4cea6b16d3e03c9b2a236ecb107/stale-ec2.jpg)](https://youtu.be/BgyYqUXuHuk?si=Gi6vkxhnVJQBILkG)
+
+
+## Production-Grade EC2 Auto Start–Stop System for Cost Optimization Using AWS Serverless Services
+
+### Tags
+`Autostartstop = True`
+
+### lambda time out
+5 min  
+Runtime python3.14
+
+### IAM ROLE inline policy
+
+- `ec2:DescribeInstances`
+- `ec2:StartInstances`
+- `ec2:StopInstances`
+- `sns:Publish`
+
+
+### test case
+- `test->action=stop`
+- `test->action=start`
+
+### event bridge scheduler
+start at 8 am  
+stop at 6 pm
+
+### start scheduler
+`00 8 ? JAN-DEC MON-FRI *`
+
+### stop scheduler
+`00 18 ? JAN-DEC MON-FRI *`
+
+### payload
+```json
+{
+    "action": "stop"
+}
+```
+
+### payload
+```json
+{
+    "action": "start"
+}
+```
+
+
+### Delete the resources
+
+1. delete the lambda function
+2. delete the iam role
+3. delete the event bridge scheduler
+4. delete the sns topic
+5. delete the ec2 instances
